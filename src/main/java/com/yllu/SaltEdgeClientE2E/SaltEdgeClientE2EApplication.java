f75b9907a2be46e01506a12b31cf70cfcacde327
bogdan.mitroiu <mitroiu.bogdan@gmail.com>
2023-04-02 18:37:48 +0300

    Initial commit

---
+++ src/main/java/com/yllu/SaltEdgeClientE2E/SaltEdgeClientE2EApplication.java
@@ -0,0 +1,13 @@
+package com.yllu.SaltEdgeClientE2E;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaltEdgeClientE2EApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaltEdgeClientE2EApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yllu/SaltEdgeClientE2E/SaltEdgeClientE2EApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yllu.SaltEdgeClientE2E;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaltEdgeClientE2EApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
